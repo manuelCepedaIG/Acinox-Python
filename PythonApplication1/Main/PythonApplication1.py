2c7841ca07306b09c70dc038921cf645df9b6ac5
mcepeda <mcepeda@intergrupo.com>
2020-11-10 12:54:27 -0500

    mejoras de codigo

--- PythonApplication1/Main/PythonApplication1.py
+++ PythonApplication1/Main/PythonApplication1.py
@@ -1,13 +1,20 @@
 from Bussiness import SQLBussiness
-
+from Bussiness import XMLBussiness
+import datetime
 
 def main():
     print("WELCOME TO ACINOX-PYTHON PROJECT \n\n")
 
-    conn = SQLBussiness.SQLClass.connect()
-    SQLBussiness.SQLClass.getQueryData(conn, "sociedades", codeEntity = None)
+    fecha = datetime.datetime.now().strftime("%Y%m%d.%H%M")
     
-    print("\nDone. Press any key to close.")
+    conn = SQLBussiness.SQLClass.connect()
+    sqlResult1 = SQLBussiness.SQLClass.getQueryData(conn, "sociedades", "")
+    #sqlResult2 = SQLBussiness.SQLClass.getQueryData(conn, "contactos", "01")
+
+    XMLBussiness.XMLClass.GenerateXML(sqlResult1, "sociedades", fecha)
+    #XMLBussiness.XMLClass.GenerateXML(sqlResult2, "clientes", fecha)
+   
+    
 
 if __name__ == '__main__':
     main()
